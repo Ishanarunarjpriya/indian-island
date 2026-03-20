3aade6adbdba68073f091a20ffeaeba18e5055f3
Ishan Arun <ishanarun@MacBook-Pro.local>
2026-03-20 15:44:25 -0500

    Revert "Add modular arena island and combat system"
    This reverts commit 5141ae6afa300de6b23e8da3e9546f2f6da3904e.

--- server/modules/arena/index.js
+++
@@ -1,3 +0,0 @@
-export { createArenaRuntime } from './runtime.js';
-export { buildDefaultArenaProgress, ensureArenaProgress, sanitizeArenaProgress, snapshotArenaProgress } from './progress.js';
-export { ARENA_WORLD, MATCHMAKING } from './config.js';
